e1ba7bedd1d8ecba16cacd88de453e0a88e32713
cedricx <cedrichxia@gmail.com>
2020-04-02 17:25:12 -0400

    Revert "Update surveys_preprocessing.R"
    This reverts commit 7a667af524c16ee8a235bec912c5d8bfdbf6c4b1.

--- Preprocessing/surveys_preprocessing.R
+++ Preprocessing/surveys_preprocessing.R
@@ -1,29 +1,23 @@
 surveys_preprocessing = function(patient_name, ...){
   survey_data = list()
-  weekly_data_filepath = paste(data_filepath, patient_name, "beiwe", sep="/")
-  if(file.exists(weekly_data_filepath)){
-    weekly_names = list.files(weekly_data_filepath)
-    for (weekly_name in weekly_names){
-      surveys_data_filepath = paste(weekly_data_filepath,weekly_name,"survey_answers",sep='/')
-      if(file.exists(surveys_data_filepath)){
-        survey_names = list.files(surveys_data_filepath)
-        for(survey_name in survey_names){
-          surveys_filepath = paste(surveys_data_filepath,survey_name,sep="/")
-          specific_surveys = list.files(surveys_filepath)
-          for(specific_survey in specific_surveys){
-            specific_survey_filepath = paste(surveys_filepath,specific_survey,sep="/")
-            specific_survey_data=read.csv2(specific_survey_filepath,header=T,sep=",")
-            if(nrow(specific_survey_data)>0){
-              date = as.POSIXct(gsub("_",":",gsub(".csv","",specific_survey)),origin="1970-01-01")
-              timestamp = as.numeric(date)
-              specific_survey_data[,"survey_id"]      = survey_name
-              specific_survey_data["date"]            = date
-              specific_survey_data[,"patient_name"]   = patient_name
-              specific_survey_data[,"timestamp"]      = timestamp
-              specific_survey_data[,"date"]           = as.Date(date)
-              survey_data[[specific_survey_filepath]] = specific_survey_data
-            }
-          }
+  surveys_data_filepath = paste(data_filepath, patient_name, "survey_answers",sep="/")
+  if(file.exists(surveys_data_filepath)){
+    survey_names = list.files(surveys_data_filepath)
+    for(survey_name in survey_names){
+      surveys_filepath = paste(surveys_data_filepath,survey_name,sep="/")
+      specific_surveys = list.files(surveys_filepath)
+      for(specific_survey in specific_surveys){
+        specific_survey_filepath = paste(surveys_filepath,specific_survey,sep="/")
+        specific_survey_data=read.csv2(specific_survey_filepath,header=T,sep=",")
+        if(nrow(specific_survey_data)>0){
+          date = as.POSIXct(gsub("_",":",gsub(".csv","",specific_survey)),origin="1970-01-01")
+          timestamp = as.numeric(date)
+          specific_survey_data[,"survey_id"]      = survey_name
+          specific_survey_data["date"]            = date
+          specific_survey_data[,"patient_name"]   = patient_name
+          specific_survey_data[,"timestamp"]      = timestamp
+          specific_survey_data[,"date"]           = as.Date(date)
+          survey_data[[specific_survey_filepath]] = specific_survey_data
         }
       }
     }
