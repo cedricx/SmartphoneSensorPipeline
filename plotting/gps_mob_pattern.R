c85eb8384bb5754179114230aeaec26b3206eb0d
cedricx <cedrichxia@gmail.com>
2020-04-14 12:33:57 -0400

    Update gps_mob_pattern.R

--- plotting/gps_mob_pattern.R
+++ plotting/gps_mob_pattern.R
@@ -13,14 +13,29 @@ yrang=plotlimits(gps_mobmat_hr)$yrang
 daily_index <- daily_subsets(gps_mobmat)
 
 gps_days <- unique(gps_mobmat_hr$days)
-par(mfrow=c(6,4))
-for (gps_day in gps_days){
-  gps_mobmat_day <- gps_mobmat_hr[daily_index[[gps_day]],]
-  title = paste("ID:",substr(patient,1,6),"on",substring(weekdays.Date(as.Date(gps_day)),1,3),gps_day)
-  diminch = 10
-  plot.flights2(gps_mobmat_day,diminch = diminch, 
-                xrang = xrang, yrang = yrang,
-                title = title)
-  plot.flights2(gps_mobmat_day,diminch = diminch,
-                title = title)
+num_days <- as.numeric(as.Date(gps_days[length(gps_days)]) - as.Date(gps_days[1]))
+num_weeks = floor((num_days+4) / 7) + 1
+
+par(mfrow=c(num_weeks,7))
+# for (gps_day in gps_days){
+#   gps_weekday <- weekdays.Date(as.Date(gps_day))
+#   gps_mobmat_day <- gps_mobmat_hr[daily_index[[gps_day]],]
+#   title = paste("ID:",substr(patient,1,6),"on",substring(gps_weekday,1,3),gps_day)
+#   diminch = 10
+#   plot.flights2(gps_mobmat_day,diminch = diminch,
+#                title = title)
+# }
+
+weekdays.Date(as.Date(names(daily_index)[1]))
+for (gps_day in names(daily_index)){
+  if (!nchar(gps_day)>0) {
+    plot.new()
+  } else {
+    gps_mobmat_day <- gps_mobmat_hr[daily_index[[gps_day]],]
+    title = paste("ID:",substr(patient,1,6),"on",substring(weekdays.Date(as.Date(gps_day)),1,3),gps_day)
+    diminch = 10
+    plot.flights2(gps_mobmat_day,diminch = diminch,
+                  title = title)
+    }
+  
 }
