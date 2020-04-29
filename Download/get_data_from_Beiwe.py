83077b35ed011ccfd17d4e2e0b926596182e4a8f
cedricx <cedrichxia@gmail.com>
2020-04-29 13:58:33 -0400

    Update get_data_from_Beiwe.py

--- Download/get_data_from_Beiwe.py
+++ Download/get_data_from_Beiwe.py
@@ -1,27 +1,26 @@
 
-crypt.py --encrypt /Volumes/Storage/Beiwe/ted_bpd.json  --output-file /Volumes/Storage/Beiwe/ted_bpd.enc
+## creat .json file and encrypt ##
+# crypt.py --encrypt /Volumes/Storage/Beiwe/ted_bpd.json  --output-file /Volumes/Storage/Beiwe/ted_bpd.enc
 
+## now in iPython ##
 import mano 
-Keyring = mano.keyring(deployment='beiwe.pennlinc',
-	keyring_file='/Volumes/Storage/Beiwe/ted_bpd.enc',
-	passphrase='peNNLinC_2020')
-
-for study in mano.studies(Keyring): 
-     print(study)
+Keyring = mano.keyring(deployment='beiwe.pennlinc', #entities in the .json file
+	keyring_file='/Volumes/Storage/Beiwe/ted_bpd.enc', #encrypted loggin credentials
+	passphrase='peNNLinC_2020') #this password needs to match the one used in the encryption process above
 
 import logging
 import mano.sync as msync
 logging.basicConfig(level=logging.INFO)
 
 output_folder = '/Volumes/Storage/Beiwe/'
-
-for study in mano.studies(Keyring):
-	_,study_id = study
-	print(study_id)
+num_subj = len(list(mano.studies(Keyring)))
+for i, study in enumerate(mano.studies(Keyring), start=1):
+	study_name,study_id = study
+	print(i,"/",num_subj,":",study_name) #help track progress
 	for user_id in mano.users(Keyring, study_id):
-		print(user_id)
+		print(user_id) #usually one id in each study
 		if os.path.exists(os.path.join(output_folder,user_id)):
-			print(user_id,'exists')
+			print(user_id,'exists') #skip downloading if already exists
 		else:
 			print(user_id,'downloading')
 			zf = msync.download(Keyring, study_id, user_id)
